f90dce7da43eeb59d12a3eaadc4903f20ceabbbc
Marcin <marcin96semkowski@gmail.com>
2019-10-28 19:30:12 +0100

    Photo

--- app/src/main/java/DemoAndroid/com/MainActivity.java
+++ app/src/main/java/DemoAndroid/com/MainActivity.java
@@ -4,14 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    public void clickFunction(View view){
-        EditText nameText = findViewById(R.id.nameText);
-        Toast.makeText(MainActivity.this, nameText.getText().toString() + "!", Toast.LENGTH_SHORT).show();
+    public void catButton(View view){
+             ImageView catView = findViewById(R.id.catView);
+             catView.setImageResource(R.drawable.cat2);
     }
 
     @Override
