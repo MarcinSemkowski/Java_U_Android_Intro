eb8e5535fc1080e815fc0b6965376989a6fc3253
Marcin <marcin96semkowski@gmail.com>
2019-11-14 17:57:02 +0100

    Song Demo #2  Music: www.bensound.com

--- app/src/main/java/DemoAndroid/com/MainActivity.java
+++ app/src/main/java/DemoAndroid/com/MainActivity.java
@@ -4,22 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private  MediaPlayer mPlayer;
+
+   public void playAudio(View view){
+
+       mPlayer.start();
+
+       // "Music: www.bensound.com"
+   }
+
+   public void pauseAudio(View view){
+       mPlayer.pause();
+   }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        MediaPlayer mPlayer = MediaPlayer.create(this,R.raw.song);
-
-        mPlayer.start();
-
-        // "Music: www.bensound.com"
-
+        mPlayer = MediaPlayer.create(this,R.raw.song);
     }
 
 }
