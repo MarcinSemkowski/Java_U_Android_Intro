ed4afac9c4acefedd6fb18a1b2980a63f3ace754
Marcin <marcin96semkowski@gmail.com>
2019-11-15 15:24:27 +0100

    List Views Demo #2

--- app/src/main/java/DemoAndroid/com/MainActivity.java
+++ app/src/main/java/DemoAndroid/com/MainActivity.java
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +20,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ListView listView = findViewById(R.id.myListView);
+
+        ArrayList<String> myFamily = new ArrayList();
+        myFamily.add("Rob");
+        myFamily.add("Kristen");
+        myFamily.add("Tommy");
+        myFamily.add("Ralphie");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,myFamily);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(
+
+                (parent, view, position, id) -> {
+                    Log.i("Person tapped: " , myFamily.get(position));
+
+                }
+
+        );
 
     }
 }
