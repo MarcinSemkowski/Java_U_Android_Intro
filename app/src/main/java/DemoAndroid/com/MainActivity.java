9ef526eed8353daea5f11e53581e534560716ccd
Marcin <marcin96semkowski@gmail.com>
2019-12-23 20:04:10 +0100

    Android Video intro #2

--- app/src/main/java/DemoAndroid/com/MainActivity.java
+++ app/src/main/java/DemoAndroid/com/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 import android.widget.ArrayAdapter;
@@ -20,10 +21,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        
+           final Handler handler = new Handler();
+            Runnable run =  new Runnable() {
+                @Override
+                public void run() {
+                  // insert code to be run every seconds
+
+                    Log.i("Runnable has run !", "a second must have passed");
+                  handler.postDelayed(this,1000);
+                }
+            };
+            handler.post(run);
            }
 
 
     }
-}
